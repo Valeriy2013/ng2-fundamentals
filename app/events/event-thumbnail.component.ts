eb95583bc4863108cb333df7bd953752dda6a596
Valeriy Sokolovskiy <4255@i.ua>
2017-03-18 13:03:51 +0200

    add service to load event content and toast service

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,13 +4,27 @@ import { Component, Input } from '@angular/core'
     selector: `event-thumbnail`,
     template: `
         <div class = "well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div [ngClass] = "getStartTimeClass()" [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late start)</span>
+                <span *ngSwitchDefault>(Normal start)</span>
+            </div>
             <div>Price: \${{event.price}}</div>
+            <div *ngIf = "event?.location">
+                <span>Location: {{event?.location?.address}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+            </div>
+            <div *ngIf = "event?.onlineUrl">
+                OnlineUrl: {{event?.onlineUrl}}
+            </div>
         </div>
     `,
     styles: [`
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
         .well div { color: #bbb; }
         .pad-left { margin-left: 10px; }
         .thumbnail { min-height: 210px; }
@@ -18,5 +32,7 @@ import { Component, Input } from '@angular/core'
 })
 export class EventThumbnailComponent{
    @Input() event: any;
-
+   getStartTimeClass(){        
+        return { green: this.event.time === '8:00 am', bold: this.event.time === '8:00 am' }
+   }
 }
